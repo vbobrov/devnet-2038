35d2a8dd0813fffa4aebd97889bd5a093211bbf0
Viktor Bobrov (vibobrov) <vibobrov@cisco.com>
2025-05-08 13:50:13 -0400

    added endpoint with rest

--- 15_rest_endpoints_cert.py
+++ 15_rest_endpoints_cert.py
@@ -1,18 +1,16 @@
-# Reference: https://developer.cisco.com/docs/pxgrid/subscribing-to-posture-topic/
-
-# As of May 6, 2025 and ISE 3.4 P1, this code does not work
+# Reference: https://github.com/cisco-pxgrid/pxgrid-rest-ws/wiki/Endpoint
 
 import requests
 import json
 from config import *
 
-# Lookup posture service
+# Lookup endpoint service
 r=requests.post(f"{pxgrid_url}/ServiceLookup",
     cert=(pxgrid_cert,pxgrid_key),
     verify=pxgrid_ca,
     auth=("pxgrid-client","none"),
     json={
-        "name": "com.cisco.ise.posture"
+        "name": "com.cisco.ise.endpoint"
     }
 )
 
@@ -28,24 +26,18 @@ service_info=r.json()["services"][0]
 # Get the restBaseUrl
 rest_url=service_info["properties"]["restBaseUrl"]
 
-# Get postured MACs
-r=requests.post(f"{rest_url}/getPosturedMacs",
+# Get endpoints
+r=requests.post(f"{rest_url}/getEndpoints",
     cert=(pxgrid_cert,pxgrid_key),
     verify=pxgrid_ca,
     auth=("pxgrid-client","none"),
-    json={}
-)
-# Raise an exception for HTTP errors
-r.raise_for_status()
-
-# Get postured MACs
-r=requests.post(f"{rest_url}/getPostureDataByMacList",
-    cert=(pxgrid_cert,pxgrid_key),
-    verify=pxgrid_ca,
-    auth=("pxgrid-client","none"),
-    json={
-        "macAddresses": ["32:23:39:AC:57:F6"],
-        "category": "hardware",
+    json={ 
+        "order":"asc",
+        "startCreateTimestamp":"2024-07-20T00:00:00.000+05:30",
+        "startUpdateTimestamp":"2025-07-20T00:00:00.000+05:30",
+        "startIndex":0,
+        "count":100,
+        "filter": "macAddress==9B:E5:27:32:6D:7A"
     }
 )
 
